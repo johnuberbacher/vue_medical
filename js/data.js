44d6a0c62e9b5fee8c5a2fc1f765ca1c012ed502
John Uberbacher <johnuberbacher@gmail.com>
2021-03-26 16:08:44 -0600

    UI updates & started pulling in doctor data

--- js/data.js
+++ js/data.js
@@ -3,18 +3,18 @@ let db = firebase.firestore()
 new Vue({
   el: '#app',
   data: {
-    locations: [],
-    reviews: [],
-    users: [],
-    inputLocationName: null,
-    inputLocationCity: null,
-    inputLocationCountry: null,
-    inputLocationSummary: null,
-    inputLocationBackdropPath: null,
-    inputReviewUser: null,
-    inputReviewBody: null,
-    inputReviewLocation: null,
-    selectedLocation: '',
+    doctors: [],
+    inputDoctorFirstName: null,
+    inputDoctorLastName: null,
+    inputDoctorSpeciatly: null,
+    inputDoctorMedicalEducation: null,
+    inputDoctorInternship: null,
+    inputDoctorResidency: null,
+    inputDoctorFellowship: null,
+    inputDoctorImagePath: null,
+    inputDoctorRank: null,
+    inputDoctorBiography: null,
+    selectedDoctor: '',
     selectedRoute: '',
   },
   methods: {
@@ -29,10 +29,10 @@ new Vue({
           this.fetch()
         })
     },
-    fetchUsers() {
-    console.log("Fetching Users");
-      return db.collection("users")
-        .orderBy('firstName')
+    fetchDoctors() {
+    console.log("Fetching Doctors");
+      return db.collection("doctors")
+        .orderBy('lastName')
         .get()
         .then(querySnapshot =>
           querySnapshot.docs.map(doc => {
@@ -41,120 +41,56 @@ new Vue({
               id: doc.id,
               firstName: data.firstName,
               lastName: data.lastName,
-              fullName: data.fullName,
+              prefix: data.prefix,
+              specialty: data.specialty,
+              medicalEducation: data.medicalEducation,
+              internship: data.internship,
+              residency: data.residency,
+              fellowship: data.fellowship,
+              imagePath: data.imagePath,
+              rank: data.rank,
+              biography: data.specialty,
             }
           })
          )
-        .then(users => this.users = users)
+        .then(doctors => this.doctors = doctors)
     },
-    fetchReviews() {
-    return db.collection("reviews")
-    .orderBy('reviewUser')
-    .get()
-    .then(querySnapshot =>
-      querySnapshot.docs.map(doc => {
-         console.log("Fetching Reviews");
-        let data = doc.data()
-        return {
-          id: doc.id,
-          reviewUser: data.reviewUser,
-          reviewBody: data.reviewBody,
-          reviewLocation: data.reviewLocation,
-          created: data.created,
-        }
-      })
-     )
-    .then(reviews => this.reviews = reviews)
-    },
-    addReview() {
-      return db.collection('reviews')
-        .add({ reviewUser: this.inputReviewUser, reviewBody: this.inputReviewBody , reviewLocation: this.inputReviewLocation, created: firebase.firestore.FieldValue.serverTimestamp()})
+    addDoctor() {
+      return db.collection('doctors').add({ firstName: this.inputfirstName, lastName: this.inputlastName, created: firebase.firestore.FieldValue.serverTimestamp(), firstName: this.inputfirstName, lastName: this.inputlastName, })
         .then(() => {
-          alert(`New Review Saved: review by ${this.inputReviewUser} for ${this.inputReviewLocation}`)
+          alert(`New Doctor Saved: ${this.inputlastName}`)
           this.clearModals()
-          this.fetchReviews()
+          this.fetchDoctors()
         })
     },
-    updateReview(review) {
-      return db.collection('reviews').doc(review.id)
-        .set({ reviewUser: review.reviewUser, reviewBody: review.reviewBody , reviewLocation: review.reviewLocation })
+    updateDoctor(doctor) {
+      return db.collection('doctors').doc(doctor.id)
+        .set({ firstName: doctor.firstName, lastName: doctor.lastName,})
         .then(() => {
-          alert(`Updated Review: ${review.reviewLocation} - &{review.reviewUser} - ${review.id}`)
-          this.clearModals()
-          this.fetchReviews()
+          alert(`Updated Doctor:  ${doctor.prefix} ${doctor.firstName} ${doctor.lastName} - ${doctor.id}`)
+          return this.fetchDoctors()
         })
     },
-    deleteReview(id) {
+    deleteDoctor(id) {
       var txt;
       var accept = confirm("You are about to delete this record, are you sure?");
       if (accept == true) {
-          return db.collection('reviews').doc(id)
+          return db.collection('doctors').doc(id)
             .delete()
             .then(() => {
-              return this.fetchLocations()
-            })
-      }
-    },
-    fetchLocations() {
-    console.log("Fetching Locations");
-      return db.collection("locations")
-        .orderBy('locationName')
-        .get()
-        .then(querySnapshot =>
-          querySnapshot.docs.map(doc => {
-            let data = doc.data()
-            return {
-              id: doc.id,
-              locationName: data.locationName,
-              summary: data.summary,
-              rateUSD: data.rateUSD,
-              city: data.city,
-              country: data.country,
-              backdropPath: data.backdropPath,
-              created: data.created,
-            }
-          })
-         )
-        .then(locations => this.locations = locations)
-    },
-    addLocation() {
-      return db.collection('locations').add({  locationName: this.inputLocationName, created: firebase.firestore.FieldValue.serverTimestamp(), locationName: this.inputLocationName, city: this.inputLocationCity , country: this.inputLocationCountry, backdropPath: this.inputLocationBackdropPath, summary: this.inputLocationSummary})
-        .then(() => {
-          alert(`New Location Saved: ${this.inputLocationName} - ${this.inputLocationCity}, ${this.inputLocationCountry}`)
-          this.clearModals()
-          this.fetchLocations()
-        })
-    },
-    updateLocation(location) {
-      return db.collection('locations').doc(location.id)
-        .set({ locationName: location.locationName, city: location.city , country: location.country, backdropPath: location.backdropPath, rateUSD: location.rateUSD, summary: location.summary})
-        .then(() => {
-          alert(`Updated Location:  ${location.locationName} - ${location.id}`)
-          return this.fetchLocations()
-        })
-    },
-    deleteLocation(id) {
-      var txt;
-      var accept = confirm("You are about to delete this record, are you sure?");
-      if (accept == true) {
-          return db.collection('locations').doc(id)
-            .delete()
-            .then(() => {
-              return this.fetchLocations()
+              return this.fetchDoctors()
             })
       }
     },
 
   },
   created() {
-    this.fetchLocations();
-    this.fetchUsers();
-    this.fetchReviews();
+    this.fetchDoctors();
   },
 })
 
 async function sample() {
-    await delay(4000);
+    await delay(1000);
     console.log("This is the delay")
     //document.getElementById('send_id').click();
 }
